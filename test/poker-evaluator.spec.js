4662aa198621beefb728ebb67ab4f0a0ec80d1b7
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-10 02:36:31 +0300

    refactor: fix error message and add units for throw about wrong card count

--- test/poker-evaluator.spec.js
+++ test/poker-evaluator.spec.js
@@ -1,7 +1,28 @@
 const PokerEvaluator = require('../lib/PokerEvaluator');
 
-describe('evalHand', function () {
-  it('5 cards, full house', function () {
+describe('evalHand', function() {
+  describe('should throw', function() {
+    it('if 4 cards', function() {
+      expect(function() {
+        PokerEvaluator.evalHand(['As', 'Ac', 'Ad', '5s']);
+      }).toThrow();
+    });
+    it('if 8 cards', function() {
+      expect(function() {
+        PokerEvaluator.evalHand([
+          'As',
+          'Ac',
+          'Ad',
+          '5s',
+          'Ad',
+          'Ah',
+          '5c',
+          '5s'
+        ]);
+      }).toThrow();
+    });
+  });
+  it('5 cards, full house', function() {
     expect(PokerEvaluator.evalHand(['As', 'Ac', 'Ad', '5d', '5s'])).toEqual({
       handType: 7,
       handRank: 148,
@@ -9,7 +30,7 @@ describe('evalHand', function () {
       handName: 'full house'
     });
   });
-  it('3 cards, one pair', function () {
+  it('3 cards, one pair', function() {
     expect(PokerEvaluator.evalHand(['As', 'Ac', 'Qs'])).toEqual({
       handType: 2,
       handRank: 2761,
@@ -17,7 +38,7 @@ describe('evalHand', function () {
       handName: 'one pair'
     });
   });
-  it('7 cards, straight flush', function () {
+  it('7 cards, straight flush', function() {
     expect(
       PokerEvaluator.evalHand(['As', 'Ks', 'Qs', 'Js', 'Ts', '3c', '5h'])
     ).toEqual({
