aeb5e3755e728e9c58593ec31b8010fdba266133
rorymcgit <rjmcguinness@gmail.com>
2020-02-09 15:20:39 +0000

    Fix fs + path node imports to es6 import. Bump version 1.0.1

--- src/poker-evaluator.ts
+++ src/poker-evaluator.ts
@@ -1,5 +1,5 @@
-import fs = require('fs');
-import path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
 
 import { DECK, HANDTYPES } from './constants';
 import ThreeCardConverter = require('./three-card-converter');
